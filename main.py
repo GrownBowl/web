7b0929a91d7fc3b18f006d81d90b3abd47d7ecf3
GrownBowl <retsya.erno@gmail.com>
2024-03-07 16:13:49 +0300

    Задание Список профессий

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ def index(text):
     return render_template("base.html", title=text)
 
 
+@app.route('/list_prof/<type_list>')
+def list_prof(type_list):
+    lists = ["инженер-исследователь", "пилот", "строитель", "экзобиолог", "врач", "инженер по терраформированию",
+             "климатолог", "специалист по радиационной защите", "астрогеолог", "гляциолог", "инженер жизнеобеспечения",
+             "метеоролог", "оператор марсохода", "киберинженер", "штурман", "пилот джронов"]
+    return render_template("list_prof.html", profs=lists, types=type_list)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
