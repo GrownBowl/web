4e261ad57ee66a1fe8e442767b7f4f9e325f0888
GrownBowl <retsya.erno@gmail.com>
2024-03-10 14:44:14 +0300

    Задание Автоматический ответ

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -14,6 +14,15 @@ def list_prof(type_list):
              "климатолог", "специалист по радиационной защите", "астрогеолог", "гляциолог", "инженер жизнеобеспечения",
              "метеоролог", "оператор марсохода", "киберинженер", "штурман", "пилот джронов"]
     return render_template("list_prof.html", profs=lists, types=type_list)
+
+
+@app.route('/auto_answer')
+def answer():
+    person = {'surname': 'Рётся', 'name': 'Эрно', 'education': 'Основное общее',
+              'profession': 'в процессе получения', 'sex': 'М', 'motivation': 'Нету',
+              'ready': 'False'}
+    return render_template("auto_answer.html", title="Анкета", person=person,
+                           css=url_for('static', filename='css/anwser_style.css'))
 
 
 if __name__ == '__main__':
