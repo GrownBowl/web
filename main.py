f0c1e40a213c937db043806caa1dbf639db9f24b
GrownBowl <retsya.erno@gmail.com>
2024-03-12 19:18:13 +0300

    Из материалов урока "Знакомство с flask-sqlalchemy"

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
-from flask import Flask, render_template, url_for, redirect
-
-from loginform import LoginForm
+from flask import Flask, render_template, redirect
+from data import db_session
+from data.news import News
+from data.users import User
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/<string:text>')
-def index(text):
-    return render_template("base.html", title=text)
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
 
 
-@app.route('/list_prof/<type_list>')
-def list_prof(type_list):
-    lists = ["инженер-исследователь", "пилот", "строитель", "экзобиолог", "врач", "инженер по терраформированию",
-             "климатолог", "специалист по радиационной защите", "астрогеолог", "гляциолог", "инженер жизнеобеспечения",
-             "метеоролог", "оператор марсохода", "киберинженер", "штурман", "пилот джронов"]
-    return render_template("list_prof.html", profs=lists, types=type_list)
-
-
-@app.route('/auto_answer')
-def answer():
-    person = {'surname': 'Рётся', 'name': 'Эрно', 'education': 'Основное общее',
-              'profession': 'в процессе получения', 'sex': 'М', 'motivation': 'Нету',
-              'ready': 'False'}
-    return render_template("auto_answer.html", title="Анкета", person=person,
-                           css=url_for('static', filename='css/anwser_style.css'))
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
     if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Авторизация', form=form)
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run()
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
